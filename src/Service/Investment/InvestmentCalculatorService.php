482ad606f56a9318f6cd3e69abdfad91452aec32
Arthuz2 <arthur.porcino43@gmail.com>
2026-02-24 22:24:14 -0300

    feat(investment): add investment tax calculation

--- src/Service/Investment/InvestmentCalculatorService.php
+++ src/Service/Investment/InvestmentCalculatorService.php
@@ -30,4 +30,28 @@ class InvestmentCalculatorService
 
         return round($balance, 2);
     }
+
+    public function calculateTax(
+        float $investedAmount,
+        float $balance,
+        \DateTimeImmutable $startDate,
+        \DateTimeImmutable $endDate,
+    ): float {
+        $gain = max(0, $balance - $investedAmount);
+
+        $years = $startDate->diff($endDate)->y;
+
+        if ($years < 1) {
+            $taxRate = 0.225;
+        } elseif (1 <= $years && $years <= 2) {
+            $taxRate = 0.185;
+        } elseif ($years > 2) {
+            $taxRate = 0.15;
+        }
+
+        $discount = $gain - ($gain * $taxRate);
+        $finalBalance = $investedAmount + $discount;
+
+        return round($finalBalance, 2);
+    }
 }
